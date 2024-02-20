fb5115beb085d1155d116af48a4495cd0a540012
satvikj5 <159411306+satvikj5@users.noreply.github.com>
2024-02-20 08:48:51 +0530

    Create boxarea.h

---
+++ week3/boxarea.h
@@ -0,0 +1,7 @@
+boxArea.h
+#include<iostream>
+using namespace std;
+void boxArea(float length,float height,float width)
+{
+    cout<<"area: "<<2*length*height+2*length*width+2*height*width<<endl;
+}
