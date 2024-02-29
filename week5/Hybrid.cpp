6ec3a779dfa324d3165f63f40e81e1574155fbc4
satvikj5 <159411306+satvikj5@users.noreply.github.com>
2024-02-29 11:54:30 +0530

    Add files via upload

---
+++ week5/Hybrid.cpp
@@ -0,0 +1,34 @@
+//HybridInheritance
+
+#include <iostream> 
+using namespace std;  
+class Vehicle  
+{ 
+  public: 
+    Vehicle() 
+    { 
+      cout<<"This is a Vehicle" << endl; 
+    } 
+}; 
+class Fare 
+{ 
+    public: 
+    Fare() 
+    { 
+        cout<<"Fare of Vehicle\n"; 
+    } 
+}; 
+class Car: public Vehicle 
+{ 
+  
+}; 
+class Bus: public Vehicle, public Fare 
+{ 
+      
+}; 
+int main() 
+{    
+    Bus obj2; 
+    return 0; 
+}
+
---
+++ week5/Simple.cpp
@@ -0,0 +1,19 @@
+//Single Inheritance
+#include<iostream> 
+using namespace std; 
+class Vehicle { 
+  public: 
+    Vehicle() 
+    { 
+      cout<<"This is a Vehicle"<<endl; 
+    } 
+}; 
+
+class Car: public Vehicle{ 
+  
+}; 
+int main() 
+{    
+    Car obj; 
+    return 0; 
+}
